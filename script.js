4eaf23453a9abcdaafa2cf17eb93f12b560df78e
ahmedwesl <ahmed.oueslati@le-campus-numerique.fr>
2023-03-13 16:56:12 +0100

    labyrinthe visu

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 // Sélectionnez l'élément parent qui contiendra la grille
 let parent = document.getElementById('grid-container');
 
-// Créez une boucle pour créer 100 cases (10x10)
-for (let i = 0; i < 100; i++) {
-// Créez un élément div pour chaque case
-    let cell = document.createElement('div');
-    cell.classList.add('cell'); // Ajoutez une classe pour styliser la case
-    cell.setAttribute('id', 'cell-' + (i + 1)); // Ajoutez un ID unique à chaque div
-// Ajoutez un numéro à chaque div
-    cell.innerText = i + 1;
+// Créer une boucle pour créer 9 conteneurs de 3 cases chacun
+for (let j = 0; j < 3; j++) {
+    // Créer un élément div pour chaque conteneur de ligne
+    let row = document.createElement('div');
+    row.classList.add('row'); // Ajouter une classe pour styliser la ligne
 
-// Ajoutez la case au parent
-    parent.appendChild(cell);
+    // Créer une boucle pour créer 3 cases dans chaque ligne
+    for (let i = 0; i < 3; i++) {
+        // Créer un élément div pour chaque case
+        let cell = document.createElement('div');
+        cell.classList.add('cell'); // Ajouter une classe pour styliser la case
+        cell.setAttribute('id', 'cell-' + ((j * 3) + i + 1)); // Ajouter un ID unique à chaque div
+        // Ajouter un numéro à chaque div
+        cell.innerText = (j * 3) + i + 1;
 
-// Si i est un multiple de 10, ajoutez une classe pour revenir à la ligne
-    if ((i + 1) % 10 === 0) {
-        cell.classList.add("new-line");
+        // Ajouter la case au parent
+        row.appendChild(cell);
     }
+
+    // Ajouter la ligne au parent
+    parent.appendChild(row);
 }
+
+
+var tableau = data.labyrinthe[0]["3"]["ex-0"];
+
+
+tableau.forEach(function(element) {
+
+    var cellId = "cell-" + ((element.posY * 3) + element.posX + 1);
+
+
+    var cellDiv = document.querySelector("#" + cellId);
+
+
+    if (element.walls[0]) {
+        cellDiv.style.borderTop = "3px solid purple";
+    }
+    if (element.walls[1]) {
+        cellDiv.style.borderRight = "3px solid purple";
+    }
+    if (element.walls[2]) {
+        cellDiv.style.borderBottom = "3px solid purple";
+    }
+    if (element.walls[3]) {
+        cellDiv.style.borderLeft = "3px solid purple";
+    }
+     if (element.entrance == true ) {
+         cellDiv.style.backgroundColor = " red";
+    }
+
+    if (element.exit == true ) {
+        cellDiv.style.backgroundColor = " green";
+    }
+});
+
