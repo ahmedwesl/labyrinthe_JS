c616eb03c98e30d9c51b0e51b290de463cc17cc2
ahmedwesl <ahmed.oueslati@le-campus-numerique.fr>
2023-03-14 11:50:25 +0100

    labyrinthe avec differentes tailles

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+let size=5;
+
 // Sélectionnez l'élément parent qui contiendra la grille
 let parent = document.getElementById('grid-container');
 
-// Créer une boucle pour créer 9 conteneurs de 3 cases chacun
-for (let j = 0; j < 3; j++) {
+
+for (let j = 0; j < size; j++) {
     // Créer un élément div pour chaque conteneur de ligne
     let row = document.createElement('div');
     row.classList.add('row'); // Ajouter une classe pour styliser la ligne
 
     // Créer une boucle pour créer 3 cases dans chaque ligne
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < size; i++) {
         // Créer un élément div pour chaque case
         let cell = document.createElement('div');
         cell.classList.add('cell'); // Ajouter une classe pour styliser la case
-        cell.setAttribute('id', 'cell-' + ((j * 3) + i + 1)); // Ajouter un ID unique à chaque div
+        cell.setAttribute('id', 'cell-' + ((j * size) + i + 1)); // Ajouter un ID unique à chaque div
         // Ajouter un numéro à chaque div
-        cell.innerText = (j * 3) + i + 1;
+        cell.innerText = (j * size) + i + 1;
 
         // Ajouter la case au parent
         row.appendChild(cell);
@@ -25,12 +27,12 @@ for (let j = 0; j < 3; j++) {
 }
 
 
-var tableau = data.labyrinthe[0]["3"]["ex-0"];
+var tableau = data.labyrinthe[0][size]["ex-1"];
 
 
 tableau.forEach(function(element) {
 
-    var cellId = "cell-" + ((element.posY * 3) + element.posX + 1);
+    var cellId = "cell-" + ((element.posY * size) + element.posX + 1);
 
 
     var cellDiv = document.querySelector("#" + cellId);
@@ -56,4 +58,3 @@ tableau.forEach(function(element) {
         cellDiv.style.backgroundColor = " green";
     }
 });
-
